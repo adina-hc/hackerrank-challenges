eb1d8a605f7ce856b7186f1ce384db7713201121
Adina Henderson <adinahenderson.mail@gmail.com>
2021-10-21 18:24:46 -0400

    corrected code

--- javascript/Day-5.js
+++ javascript/Day-5.js
@@ -1,1 +1,9 @@
 // Loops
+
+function multiples(intNum) {
+  for (let i = 1; i < 11; i++) {
+    let result = intNum * i;
+    console.log(`${intNum} X ${i} = ${result}`);
+  }
+}
+return multiples(3);
