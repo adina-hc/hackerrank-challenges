332b21e52404857266b4c3d105ad351c92c09237
Adina Henderson <adinahenderson.mail@gmail.com>
2021-12-01 10:37:55 -0500

    switch statement

---
+++ javascript/10-days-JS/10d-loops.js
@@ -0,0 +1,13 @@
+// Loops
+
+let s="";
+const vowels = ['a','e','i','o','u'];
+const consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
+
+function vowelsAndConsonants(s) {
+    for (let i=1;s.length;i++) {
+        vowels.map()
+    }
+
+
+}
