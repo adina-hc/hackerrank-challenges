34c473d482f9877087fc9f71027d66d8c25a6710
Adina Henderson <adinahenderson.mail@gmail.com>
2021-12-01 10:38:57 -0500

    arithmetic operations

---
+++ javascript/arithmetic/arithmetic.js
@@ -0,0 +1,18 @@
+
+// Arithmetic functions to play with 2 numbers
+
+function add(num1, num2) {
+    return num1+num2;
+}
+
+function subtract (num1, num2) {
+    return num1-num2;
+}
+
+function multiply (num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
